cb30b3009de70264c34d5fc5a82d4d613171c4ea
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-10-10 10:51:27 -0300

    Fixing security issue on errorsHandler

--- src/middlewares/errorsHandler.js
+++ src/middlewares/errorsHandler.js
@@ -1,6 +1,8 @@
 const errorsHandler = (err, _req, res, _next) => {
   const { status, message } = err;
-  res.status(status).json({ message });
+  const customStatus = status || 500;
+  const Message = customStatus === 500 ? 'Internal Server Error' : message;
+  res.status(customStatus).json({ Message });
 };
 
 module.exports = errorsHandler;
